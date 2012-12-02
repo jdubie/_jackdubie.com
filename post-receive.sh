c17b55360a71a4f79fe64a995327942ece4062a6
Jack Dubie <jdubie@cs.stanford.edu>
2012-12-01 17:28:02 -0800

    Modified post recieve hook

--- post-receive.sh
+++ post-receive.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 cd /home/jdubie/www/jackdubie.com
 git pull git@github.com:jdubie/jackdubie.com master
-git push origin master
+git push git@github.com:jdubie/jackdubie.com master
+git push ../jackdubie.git
